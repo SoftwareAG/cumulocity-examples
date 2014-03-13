452e1131e2887cbacfb9d5ce7cc4c42acc779b0c
Michał Kozaryna <michal.kozaryna@impaqgroup.com>
2014-03-13 13:36:53 +0100

    MTM-6265 fixed logback configuration

--- linux-agent/assembly/src/main/base-tar/c8y-agent.sh
+++ linux-agent/assembly/src/main/base-tar/c8y-agent.sh
@@ -3,7 +3,7 @@
 while true
 do
   echo "Running the Cumulocity Linux Agent..."
-  java -cp 'cfg/*:lib/*' c8y.lx.agent.Agent
+  java -cp 'cfg/*:lib/*' -Dlogback.configurationFile=cfg/logback.xml c8y.lx.agent.Agent
   sleep 1
 done
 ) 2>&1 | logger
