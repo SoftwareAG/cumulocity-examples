e284c928eea30f962c527e23adfdc37e848c04bf
Dominik Bielicki <dominik.bielicki@impaqgroup.com>
2014-03-31 16:39:20 +0200

    remove obsolete servlet

--- tracker-agent/src/main/java/c8y/trackeragent/web/TestServlet.java
+++
@@ -1,7 +0,0 @@
-package c8y.trackeragent.web;
-
-import javax.servlet.annotation.WebServlet;
-
-@WebServlet(name="mytest", urlPatterns={"/myurl"})
-public class TestServlet extends javax.servlet.http.HttpServlet {
-} 
