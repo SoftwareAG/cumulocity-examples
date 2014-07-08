7299eecb11ee59a51777e5b48839609c938113d8
Lukasz Piasecki <Lukasz.Piasecki@impaqgroup.com>
2014-07-08 17:47:01 +0200

    Updated resource

--- tixi-agent/server/src/main/java/com/cumulocity/agent/server/jaxrs/JaxrsServer.java
+++ tixi-agent/server/src/main/java/com/cumulocity/agent/server/jaxrs/JaxrsServer.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.servlet.WebappContext;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 import org.glassfish.jersey.servlet.ServletContainer;
@@ -15,9 +16,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.multipart.MultipartResolver;
 
 import com.cumulocity.agent.server.Server;
 import com.cumulocity.agent.server.context.ContextFilter;
+import com.cumulocity.tixi.server.resources.SendDataResource;
 import com.google.common.base.Throwables;
 import com.google.common.util.concurrent.AbstractService;
 import com.google.common.util.concurrent.Service;
@@ -37,13 +40,16 @@ public class JaxrsServer implements Server {
 
     private final Service service = new AbstractService() {
 
-       private HttpServer server;
+    private HttpServer server;
 
         @Override
         protected void doStart() {
             server = HttpServer.createSimpleServer(null, new InetSocketAddress(host, port));
             WebappContext context = new WebappContext(applicationId, "/" + applicationId);
-            resourceConfig.register(RequestContextFilter.class);
+            resourceConfig
+            .register(RequestContextFilter.class)
+            .register(MultiPartFeature.class);
+            System.out.println(resourceConfig.getClasses());
             context.addServlet("jersey-servlet", new ServletContainer(resourceConfig)).addMapping("/*");
             context.addFilter("deviceContextFilter", applicationContext.getBean(ContextFilter.class)).addMappingForServletNames(null,
                     "jersey-servlet");
--- tixi-agent/server/src/main/java/com/cumulocity/tixi/server/resources/SendDataResource.java
+++ tixi-agent/server/src/main/java/com/cumulocity/tixi/server/resources/SendDataResource.java
@@ -7,13 +7,11 @@ import java.io.InputStream;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.transform.stream.StreamSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.cumulocity.tixi.server.components.txml.TXMLUnmarshaller;
 import com.cumulocity.tixi.server.services.AgentFileSystem;
 import com.cumulocity.tixi.server.services.RequestStorage;
 import com.sun.jersey.core.header.FormDataContentDisposition;
@@ -24,15 +22,12 @@ public class SendDataResource {
     
     private static final Logger log = LoggerFactory.getLogger(SendDataResource.class);
 
-    private final TXMLUnmarshaller txmlUnmarshaller;
-    
     private final RequestStorage requestStorage; 
     
     private final AgentFileSystem agentFileSystem;
 
     @Autowired
-    public SendDataResource(TXMLUnmarshaller txmlUnmarshaller, RequestStorage requestStorage, AgentFileSystem agentFileSystem) {
-        this.txmlUnmarshaller = txmlUnmarshaller;
+    public SendDataResource(RequestStorage requestStorage, AgentFileSystem agentFileSystem) {
         this.requestStorage = requestStorage;
         this.agentFileSystem = agentFileSystem;
     }
@@ -40,28 +35,24 @@ public class SendDataResource {
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response senddata(@FormDataParam("file") InputStream fileInputStream,
+    public Response senddata(
+            @FormDataParam("file") InputStream fileInputStream,
             @FormDataParam("file") FormDataContentDisposition contentDispositionHeader, 
             @QueryParam("requestId") String requestId, 
             @QueryParam("serial") String serial,
             @QueryParam("user") String user,
             @QueryParam("password") String password) {
         String fileName = agentFileSystem.writeIncomingFile(requestId, fileInputStream);
-        Object requestEntity = getRequestEntity(fileName, requestId);
+        Object requestEntity = getRequestEntity(fileInputStream, requestId);
         return Response.ok(statusOKJson()).build();
     }
 
-    private Object getRequestEntity(String fileName, String requestId) {
+    private Object getRequestEntity(InputStream fileInputStream, String requestId) {
         Class<?> entityType = requestStorage.get(requestId);
         if (requestId == null || entityType == null) {
             return null;
         }
-        try {
-            return txmlUnmarshaller.unmarshal(fileName, entityType);
-        } catch (Exception e) {
-            log.error("", e);
-        }
-        return null;
+        return entityType;
     }
 
 }
