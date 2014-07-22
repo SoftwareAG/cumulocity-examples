59abe6d0ea9c6b719e95f1d753972405f1fbe6a1
Dominik Bielicki <dominik.bielicki@impaqgroup.com>
2014-07-22 12:56:19 +0200

    MTM-7109 store date of last processed log file in agent

--- tixi-agent/server/src/test/java/com/cumulocity/tixi/server/model/txml/RecordItemDefinitionBuilder.java
+++ tixi-agent/server/src/test/java/com/cumulocity/tixi/server/model/txml/RecordItemDefinitionBuilder.java
@@ -4,51 +4,51 @@ import com.cumulocity.tixi.server.components.txml.RecordItemPathAdapter;
 import com.cumulocity.tixi.server.model.txml.RecordItemDefinition;
 import com.cumulocity.tixi.server.model.txml.RecordItemPath;
 
-public class LogDefinitionItemBuilder {
+public class RecordItemDefinitionBuilder {
 
 	private final RecordItemDefinition result = new RecordItemDefinition();
 	private final RecordItemPathAdapter itemPathAdapter = new RecordItemPathAdapter();
 
-	public static LogDefinitionItemBuilder anItem() {
-		return new LogDefinitionItemBuilder();
+	public static RecordItemDefinitionBuilder anItem() {
+		return new RecordItemDefinitionBuilder();
 	}
 
-	public LogDefinitionItemBuilder withName(String name) {
+	public RecordItemDefinitionBuilder withName(String name) {
 		result.setName(name);
 		return this;
 	}
 
-	public LogDefinitionItemBuilder withType(String type) {
+	public RecordItemDefinitionBuilder withType(String type) {
 		result.setType(type);
 		return this;
 	}
 
-	public LogDefinitionItemBuilder withSize(int size) {
+	public RecordItemDefinitionBuilder withSize(int size) {
 		result.setSize(size);
 		return this;
 	}
 
-	public LogDefinitionItemBuilder withExp(int exp) {
+	public RecordItemDefinitionBuilder withExp(int exp) {
 		result.setExp(exp);
 		return this;
 	}
 
-	public LogDefinitionItemBuilder withFormat(String format) {
+	public RecordItemDefinitionBuilder withFormat(String format) {
 		result.setFormat(format);
 		return this;
 	}
 
-	public LogDefinitionItemBuilder withId(String id) {
+	public RecordItemDefinitionBuilder withId(String id) {
 		result.setId(id);
 		return this;
 	}
 	
-	public LogDefinitionItemBuilder withPath(RecordItemPath path) {
+	public RecordItemDefinitionBuilder withPath(RecordItemPath path) {
 		result.setPath(path);
 		return this;
 	}
 	
-	public LogDefinitionItemBuilder withPath(String path) throws Exception {
+	public RecordItemDefinitionBuilder withPath(String path) throws Exception {
 		return withPath(itemPathAdapter.unmarshal(path));
 	}
 
