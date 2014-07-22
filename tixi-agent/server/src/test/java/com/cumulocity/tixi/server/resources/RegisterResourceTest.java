b844119b7f07bd40cda7d00b6665759d1e41c437
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2014-07-22 17:43:38 +0200

    fixed unit test

--- tixi-agent/server/src/test/java/com/cumulocity/tixi/server/resources/RegisterResourceTest.java
+++ tixi-agent/server/src/test/java/com/cumulocity/tixi/server/resources/RegisterResourceTest.java
@@ -1,16 +1,18 @@
 package com.cumulocity.tixi.server.resources;
 
 import static org.fest.assertions.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import com.cumulocity.model.idtype.GId;
 import com.cumulocity.tixi.server.model.SerialNumber;
 import com.cumulocity.tixi.server.model.TixiDeviceCredentails;
+import com.cumulocity.tixi.server.services.DeviceControlService;
 import com.cumulocity.tixi.server.services.DeviceService;
 
 public class RegisterResourceTest {
@@ -18,7 +20,8 @@ public class RegisterResourceTest {
     @Test
     public void shouldBootstrap() {
         DeviceService deviceService = mock(DeviceService.class);
-        RegisterResource resource = new RegisterResource(deviceService);
+        DeviceControlService deviceControlService = mock(DeviceControlService.class);
+        RegisterResource resource = new RegisterResource(deviceService,deviceControlService);
         when(deviceService.register(new SerialNumber("12345"))).thenReturn(new TixiDeviceCredentails("user", "pass", "id"));
         
         Response response = resource.get("12345", null);
@@ -26,6 +29,7 @@ public class RegisterResourceTest {
         TixiRequest tixiResponse = (TixiRequest) response.getEntity();
         assertThat(response.getStatus()).isEqualTo(Status.OK.getStatusCode());
         assertThat(tixiResponse.getProperties().get("user")).isEqualTo("user");
+        verify(deviceControlService).startOperationExecutor(GId.asGId("id"));
     }
 
 }
