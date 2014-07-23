f6de45d4bb299743adb49186aa56d14fc25b1175
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2014-07-23 12:09:01 +0200

    fixed junit tests

--- tixi-agent/server/src/test/java/com/cumulocity/tixi/server/resources/CommandPipeResourceTest.java
+++ tixi-agent/server/src/test/java/com/cumulocity/tixi/server/resources/CommandPipeResourceTest.java
@@ -7,23 +7,28 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
+import com.cumulocity.model.idtype.GId;
 import com.cumulocity.tixi.server.model.TixiRequestType;
 import com.cumulocity.tixi.server.services.ChunkedOutputMessageChannel;
+import com.cumulocity.tixi.server.services.DeviceControlService;
 import com.cumulocity.tixi.server.services.DeviceMessageChannelService;
 
 public class CommandPipeResourceTest {
 
     DeviceMessageChannelService device = mock(DeviceMessageChannelService.class);
 
-    CommandPipeResource commandPipe = new CommandPipeResource(device);
+    DeviceControlService deviceControlService = mock(DeviceControlService.class);
+    
+    CommandPipeResource commandPipe = new CommandPipeResource(device,deviceControlService);
 
     @Test
     public void shouldBootstrap() {
         ArgumentCaptor<TixiRequestType> reqTypeCaptor = ArgumentCaptor.forClass(TixiRequestType.class);
 
-        commandPipe.open("some_serial", "some_user");
+        commandPipe.open("some_serial", "some_user", "deviceId");
 
         verify(device).registerMessageOutput(Mockito.any(ChunkedOutputMessageChannel.class));
+        verify(deviceControlService).startOperationExecutor(GId.asGId("deviceId"));
     }
 
 }
--- tixi-agent/server/src/test/java/com/cumulocity/tixi/server/resources/RegisterResourceTest.java
+++ tixi-agent/server/src/test/java/com/cumulocity/tixi/server/resources/RegisterResourceTest.java
@@ -1,18 +1,18 @@
 package com.cumulocity.tixi.server.resources;
 
+import static com.cumulocity.model.idtype.GId.asGId;
 import static org.fest.assertions.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.junit.Test;
-import org.mockito.Mockito;
 
-import com.cumulocity.model.idtype.GId;
+import com.cumulocity.tixi.server.model.ManagedObjects;
 import com.cumulocity.tixi.server.model.SerialNumber;
 import com.cumulocity.tixi.server.model.TixiDeviceCredentails;
-import com.cumulocity.tixi.server.services.DeviceControlService;
 import com.cumulocity.tixi.server.services.DeviceService;
 
 public class RegisterResourceTest {
@@ -20,16 +20,26 @@ public class RegisterResourceTest {
     @Test
     public void shouldBootstrap() {
         DeviceService deviceService = mock(DeviceService.class);
-        DeviceControlService deviceControlService = mock(DeviceControlService.class);
-        RegisterResource resource = new RegisterResource(deviceService,deviceControlService);
-        when(deviceService.register(new SerialNumber("12345"))).thenReturn(new TixiDeviceCredentails("user", "pass", "id"));
+        RegisterResource resource = new RegisterResource(deviceService);
+        when(deviceService.bootstrap(new SerialNumber("12345"))).thenReturn(new TixiDeviceCredentails("user", "pass", "id"));
         
         Response response = resource.get("12345", null);
         
         TixiRequest tixiResponse = (TixiRequest) response.getEntity();
         assertThat(response.getStatus()).isEqualTo(Status.OK.getStatusCode());
         assertThat(tixiResponse.getProperties().get("user")).isEqualTo("user");
-        verify(deviceControlService).startOperationExecutor(GId.asGId("id"));
+    }
+    
+    @Test
+    public void shouldRegister() {
+        DeviceService deviceService = mock(DeviceService.class);
+        RegisterResource resource = new RegisterResource(deviceService);
+        when(deviceService.register(new SerialNumber("12345"))).thenReturn(ManagedObjects.asManagedObject(asGId("id")));
+        
+        Response response = resource.get("12345", "username");
+        TixiRequest tixiResponse = (TixiRequest) response.getEntity();
+        assertThat(response.getStatus()).isEqualTo(Status.OK.getStatusCode());
+        assertThat(tixiResponse.getProperties().get("deviceID")).isEqualTo("id");
     }
 
 }
