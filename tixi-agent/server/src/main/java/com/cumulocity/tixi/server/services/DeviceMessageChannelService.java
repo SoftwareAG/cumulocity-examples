efe8685a018545a97b5fc6e7738f6b47be3df6b8
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2014-07-24 13:49:07 +0200

    update version of jersey and grizzly, added close channel when new one registred

--- tixi-agent/server/src/main/java/com/cumulocity/tixi/server/services/DeviceMessageChannelService.java
+++ tixi-agent/server/src/main/java/com/cumulocity/tixi/server/services/DeviceMessageChannelService.java
@@ -6,6 +6,7 @@ import java.util.concurrent.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,7 @@ import com.cumulocity.tixi.server.services.MessageChannel.MessageChannelListener
 
 @Component
 @DeviceScope
-public class DeviceMessageChannelService implements InitializingBean {
+public class DeviceMessageChannelService implements InitializingBean,DisposableBean {
 
     private static final Logger log = LoggerFactory.getLogger(DeviceMessageChannelService.class);
 
@@ -48,6 +49,10 @@ public class DeviceMessageChannelService implements InitializingBean {
         executorService.scheduleAtFixedRate(deviceContextService.withinContext(new SendRequestCommand()), 1, 5, TimeUnit.SECONDS);
         executorService.scheduleAtFixedRate(deviceContextService.withinContext(new EnqueueHeartBeatRequestCommand()), 1, 1, TimeUnit.MINUTES);
     }
+    @Override
+    public void destroy() throws Exception {
+        executorService.shutdown();
+    }
 
     public void send(TixiRequest tixiRequest) {
         log.debug("Enqueued tixiRequest {}.", tixiRequest);
@@ -57,6 +62,7 @@ public class DeviceMessageChannelService implements InitializingBean {
             log.warn("Enqueu  tixi request failed", e);
         }
     }
+    
 
     public void send(TixiRequestType requestType) {
         send(requestFactory.create(requestType));
@@ -64,44 +70,51 @@ public class DeviceMessageChannelService implements InitializingBean {
 
     public void registerMessageOutput(MessageChannel<TixiRequest> output) {
         log.info("Registred new output");
+        MessageChannel<TixiRequest>  previous = this.output;
         this.output = output;
+        if(isChannelActive(previous)) {
+            previous.close();
+        }
+        output.addListener(new MessageChannelListener<TixiRequest>() {
+            @Override
+            public void failed(TixiRequest message) {
+                log.debug("Failed to devlivery request {} ", message);
+                requestQueue.add(message);
+            }
+        });
+    }
+    private boolean isChannelActive(MessageChannel<TixiRequest> output) {
+        return output != null && !output.isClosed();
     }
     
     private class SendRequestCommand implements Runnable {
         public void run() {
-            if (output == null) {
+            if (!isChannelActive(output)) {
                 log.debug("no output defined");
                 return;
             }
+            final MessageChannel<TixiRequest> channel = output;
             TixiRequest request = requestQueue.poll();
             if (request == null) {
                 return;
             }
             
             log.debug("Send new tixi request {}.", request);
-            output.send(new MessageChannelListener<TixiRequest>() {
-
-                @Override
-                public void close() {
-                    output = null;
-                }
-
-                @Override
-                public void failed(TixiRequest message) {
-                    requestQueue.add(message);
-
-                }
-            }, request);
+            channel.send(request);
         }
     }
     
     private class EnqueueHeartBeatRequestCommand implements Runnable {
         public void run() {
-            if (output == null) {
+            if (!isChannelActive(output)) {
                 log.debug("no output defined");
                 return;
             }
             send(HEARTBEAT);
         }
+
+        
     }
+
+  
 }
--- tixi-agent/server/src/main/java/com/cumulocity/tixi/server/services/MessageChannel.java
+++ tixi-agent/server/src/main/java/com/cumulocity/tixi/server/services/MessageChannel.java
@@ -5,10 +5,15 @@ package com.cumulocity.tixi.server.services;
 public interface MessageChannel<T> {
     public interface MessageChannelListener<T> {
         void failed(T message);
-        void close();
     }
     
-    void send(MessageChannelListener<T> context, T message);
+    void addListener(MessageChannelListener<T> listener);
+    
+    void send(T message);
+    
+    boolean isClosed();
+
+    void close();
     
 }
     
