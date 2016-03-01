7513851bd2004518289e17e880c44ee0ba7ec1b6
Lukasz Piasecki <Lukasz.Piasecki@impaqgroup.com>
2016-03-01 15:24:37 +0100

    Fixed issue with bootstraping taking too long

--- tracker-agent/src/main/java/c8y/trackeragent/devicebootstrap/DeviceBootstrapProcessor.java
+++ tracker-agent/src/main/java/c8y/trackeragent/devicebootstrap/DeviceBootstrapProcessor.java
@@ -1,15 +1,9 @@
 package c8y.trackeragent.devicebootstrap;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import c8y.trackeragent.TrackerAgent;
@@ -17,30 +11,22 @@ import c8y.trackeragent.event.TrackerAgentEventListener;
 import c8y.trackeragent.event.TrackerAgentEvents;
 
 import com.cumulocity.rest.representation.devicebootstrap.DeviceCredentialsRepresentation;
+import com.cumulocity.sdk.client.SDKException;
 import com.cumulocity.sdk.client.devicecontrol.DeviceCredentialsApi;
-import com.cumulocity.sdk.client.polling.PollingStrategy;
 import com.google.common.eventbus.Subscribe;
 
 @Component
 public class DeviceBootstrapProcessor implements TrackerAgentEventListener {
 
-    private static final int POOL_SIZE = 2;
-
     protected static Logger logger = LoggerFactory.getLogger(DeviceBootstrapProcessor.class);
 
-    private final ExecutorService threadPoolExecutor;
-    private final Collection<String> duringBootstrap = new HashSet<String>();
-    private final Object lock = new Object();
     private final TrackerAgent trackerAgent;
     private final DeviceCredentialsApi deviceCredentialsApi;
-    private final List<Long> bootstrapPollIntervals;
 
     @Autowired
     public DeviceBootstrapProcessor(TrackerAgent trackerAgent) {
         this.trackerAgent = trackerAgent;
-        this.threadPoolExecutor = Executors.newFixedThreadPool(POOL_SIZE);
         this.deviceCredentialsApi = trackerAgent.getContext().getBootstrapPlatform().getDeviceCredentialsApi();
-        this.bootstrapPollIntervals = trackerAgent.getContext().getConfiguration().getBootstrapPollIntervals();
     }
 
     @Subscribe
@@ -50,53 +36,33 @@ public class DeviceBootstrapProcessor implements TrackerAgentEventListener {
 
     public void startBootstraping(String imei) {    
         logger.info("Start bootstrapping: {}", imei);
-        synchronized (lock) {
-            if (duringBootstrap.contains(imei)) {
-                logger.info("{} already during bootstraping; dont start it again.", imei);
-                return;
-            }
-            duringBootstrap.add(imei);
-            DeviceBootstrapTask deviceBootstrapTask = new DeviceBootstrapTask(imei);
-            threadPoolExecutor.execute(deviceBootstrapTask);
+        DeviceCredentialsRepresentation credentialsRepresentation = pollCredentials(imei);
+        if (credentialsRepresentation == null) {
+            logger.info("No credentials accessed for imei {}.", imei);
+        } else {
+            DeviceCredentials credentials = asCredentials(credentialsRepresentation);
+            logger.info("Credentials for imei {} accessed: {}.", imei, credentials);
+            trackerAgent.sendEvent(new TrackerAgentEvents.NewDeviceRegisteredEvent(credentials));
         }
     }
-
-    private class DeviceBootstrapTask implements Runnable {
-
-        private final String imei;
-
-        public DeviceBootstrapTask(String imei) {
-            this.imei = imei;
-        }
-
-        @Override
-        public void run() {
-            try {
-                doRun();
-            } finally {
-                duringBootstrap.remove(imei);
-            }
-        }
-
-        private void doRun() {
-            PollingStrategy strategy = new PollingStrategy(TimeUnit.SECONDS, bootstrapPollIntervals);
-            DeviceCredentialsRepresentation credentialsRepresentation = deviceCredentialsApi.pollCredentials(imei, strategy);
-            logger.info("Send credentials representation {}.", credentialsRepresentation);
-            if (credentialsRepresentation == null) {
-                logger.info("No credentials accessed for imei {}.", imei);
+    
+    private DeviceCredentialsRepresentation pollCredentials(final String imei) {
+        try {
+            return deviceCredentialsApi.pollCredentials(imei);
+        } catch (SDKException e) {
+            if (e.getHttpStatus() == HttpStatus.NOT_FOUND.value()) {
+                logger.debug("Crentials not yet available for device: " + imei);
             } else {
-                DeviceCredentials credentials = asCredentials(credentialsRepresentation);
-                logger.warn("Credentials for imei {} accessed: {}.", imei, credentials);
-                trackerAgent.sendEvent(new TrackerAgentEvents.NewDeviceRegisteredEvent(credentials));
+                logger.error("Failed to retrieve credentials from cumulocity.", e);
             }
         }
-
-        private DeviceCredentials asCredentials(DeviceCredentialsRepresentation credentials) {
-            //@formatter:off
-            return new DeviceCredentials(credentials.getTenantId(), credentials.getUsername(), credentials.getPassword(), null, null)
-                .setImei(credentials.getId());
-            //@formatter:on
-        }
-
+        return null;
+    }
+    
+    private DeviceCredentials asCredentials(DeviceCredentialsRepresentation credentials) {
+        //@formatter:off
+        return new DeviceCredentials(credentials.getTenantId(), credentials.getUsername(), credentials.getPassword(), null, null)
+            .setImei(credentials.getId());
+        //@formatter:on
     }
 }
