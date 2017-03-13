bfd4dc0f30b395739918bf33a83e1a5bf363d8bb
Wojciech Trzciński <wojciech.trzcinski@cumulocity.com>
2017-03-13 20:01:57 +0100

    CX-255 SMS.baseURL is not required but operation will still break while using sms mode

--- tracker-agent/src/main/java/c8y/trackeragent/sms/SmsConfiguration.java
+++ tracker-agent/src/main/java/c8y/trackeragent/sms/SmsConfiguration.java
@@ -5,10 +5,16 @@ import com.cumulocity.sms.client.SmsMessagingApiImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+
+import static org.springframework.context.annotation.ScopedProxyMode.TARGET_CLASS;
 
 @Configuration
 public class SmsConfiguration {
     @Bean
+    @Lazy
+    @Scope(proxyMode = TARGET_CLASS)
     public SmsMessagingApi smsMessagingApi(@Value("${SMS.baseURL:}") String baseUrl, OptionsAuthorizationSupplier optionsAuth) {
         return new SmsMessagingApiImpl(baseUrl, "/smsmessaging/v1", optionsAuth);
     }
