3d19ffcecd3a4fa095f614809bddb90893bc35f0
Dariusz Dudziński <dariusz.dudzinski@cumulocity.com>
2018-01-18 14:08:48 +0100

    MTM-18905 - Improvement after code review

--- hello-world-microservice/src/main/java/com/cumulocity/helloworld/HelloWorldMain.java
+++ hello-world-microservice/src/main/java/com/cumulocity/helloworld/HelloWorldMain.java
@@ -1,9 +1,7 @@
 package com.cumulocity.helloworld;
 
 import com.cumulocity.microservice.autoconfigure.MicroserviceApplication;
-import com.cumulocity.microservice.subscription.model.MicroserviceMetadataRepresentation;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +17,5 @@ public class HelloWorldMain {
     @RequestMapping("hello")
     public String greeting(@RequestParam(value = "who", defaultValue = "world") String who) {
         return "hello " + who + "!";
-    }
-
-    @Bean
-    public MicroserviceMetadataRepresentation metadata() {
-        return MicroserviceMetadataRepresentation.microserviceMetadataRepresentation().build();
     }
 }
