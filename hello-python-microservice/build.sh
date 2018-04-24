91e8feee384fe9e87864c8b8b9f431b343fb40d9
Jakub Kotorowicz <jakub.kotorowicz@cumulocity.com>
2018-04-24 12:23:49 +0200

    MTM-18163 change build and deployment mechanism, now use "microservice" script

---
+++ hello-python-microservice/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if ! command -v microservice ; then
+    wget http://resources.cumulocity.com/examples/microservice
+    chmod +x microservice
+    PATH=$PATH:.
+fi
+
+microservice pack -n hello-microservice
