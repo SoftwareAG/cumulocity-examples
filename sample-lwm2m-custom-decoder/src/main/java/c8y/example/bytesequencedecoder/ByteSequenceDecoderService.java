51807bd2055ac348b12552f3bf031877de43fbf4
Elias Weingaertner <elias.weingaertner@cumulocity.com>
2019-02-05 16:27:42 +0100

    Fix missing series Fix missing JSON serializer config

--- sample-lwm2m-custom-decoder/src/main/java/c8y/example/bytesequencedecoder/ByteSequenceDecoderService.java
+++ sample-lwm2m-custom-decoder/src/main/java/c8y/example/bytesequencedecoder/ByteSequenceDecoderService.java
@@ -7,6 +7,7 @@ import com.cumulocity.microservice.customdecoders.api.model.MeasurementValueDto;
 import com.cumulocity.microservice.customdecoders.api.service.DecoderService;
 import com.cumulocity.microservice.customdecoders.api.util.DecoderUtils;
 import com.cumulocity.model.idtype.GId;
+import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,7 @@ import java.util.Map;
 
 
 @Component
+@Slf4j
 public class ByteSequenceDecoderService implements DecoderService {
 
     /**
@@ -31,14 +33,19 @@ public class ByteSequenceDecoderService implements DecoderService {
     @Override
     public DecoderResult decode(String payloadToDecode, GId sourceDeviceId, Map<String, String> inputArguments) throws DecoderServiceException {
 
+        log.debug("Decoding payload {}. Converting hex string into values", payloadToDecode);
+
         byte[] decodedBytes = DecoderUtils.hexStringToByteArray(payloadToDecode);
+
+        log.debug("Byte values: {}",decodedBytes);
+
         DecoderResult decoderResult = new DecoderResult();
 
         List<MeasurementValueDto> measurementValueDtos = new ArrayList<>();
         int byteIndex=0;
 
         for (byte valueByte: decodedBytes) {
-
+            log.debug("Creating Measurement for byte {}, value {}", byteIndex,valueByte);
             MeasurementValueDto valueDto = new MeasurementValueDto();
             valueDto.setValue(new BigDecimal(valueByte));
             valueDto.setSeriesName("Byte "+byteIndex);
@@ -50,9 +57,10 @@ public class ByteSequenceDecoderService implements DecoderService {
         measurementDto.setType("c8y_example_lwm2m_decoder_binaryValues");
         measurementDto.setTime(new DateTime());
         measurementDto.setValues(measurementValueDtos);
-
+        measurementDto.setSeries("binaryValueSeries");
         decoderResult.addMeasurement(measurementDto);
 
+        log.debug("Finished decoding byte values");
         return decoderResult;
 
     }
---
+++ sample-lwm2m-custom-decoder/src/main/java/c8y/example/bytesequencedecoder/JacksonConfig.java
@@ -0,0 +1,21 @@
+package c8y.example.bytesequencedecoder;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper()
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.registerModule(new JodaModule());
+        return mapper;
+    }
+}
