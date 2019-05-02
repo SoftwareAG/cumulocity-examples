5dfbbb441233612e61afc04b9e1905270b29c43b
Atul Kumar Panda <atulkumar.panda@cumulocity.com>
2019-05-02 00:16:02 -0700

    Removed unused code.

--- gateway/src/main/java/com/cumulocity/snmp/utils/SimpleTypeUtils.java
+++ gateway/src/main/java/com/cumulocity/snmp/utils/SimpleTypeUtils.java
@@ -8,38 +8,6 @@ public class SimpleTypeUtils {
     public static final String GID_PREFIX = "/inventory/managedObjects/";
 
     @Nullable
-    public static String parseString(@Nullable Object object) {
-        if (object == null) {
-            return null;
-        }
-        return object.toString();
-    }
-
-    @Nullable
-    public static Long parseLong(@Nullable Object object) {
-        if (object == null) {
-            return null;
-        }
-        final String string = object.toString();
-        if (string.matches("^[-+]?\\d+$")) {
-            return Long.parseLong(string);
-        }
-        return null;
-    }
-
-    @Nullable
-    public static Integer parseInt(@Nullable Object object) {
-        if (object == null) {
-            return null;
-        }
-        final String string = object.toString();
-        if (string.matches("^[-+]?\\d+$")) {
-            return Integer.parseInt(string);
-        }
-        return null;
-    }
-
-    @Nullable
     public static GId parseGId(@Nullable Object property) {
         if (property == null) {
             return null;
