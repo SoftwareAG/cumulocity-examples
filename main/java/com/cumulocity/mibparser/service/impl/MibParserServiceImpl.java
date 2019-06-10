c314b1420e68d83dddecf8a39e4d0679de9075f0
Praveen M Vaidya <praveenm.vaidya@cumulocity.com>
2019-06-10 16:42:36 +0530

    MTM-26885 Adding licence details in every file of this project. Updated Readme file. Optimized imports. Optimized pom xml. Optimized cumulocity.json for health endpoints. Updating the url to match with url which will be called from UI as it will be a rpm/jar installation.

--- main/java/com/cumulocity/mibparser/service/impl/MibParserServiceImpl.java
+++ main/java/com/cumulocity/mibparser/service/impl/MibParserServiceImpl.java
@@ -1,18 +1,33 @@
+/*
+ * Copyright (c) 2019 Cumulocity GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.cumulocity.mibparser.service.impl;
 
 import com.cumulocity.mibparser.conversion.RegisterConversionHandler;
 import com.cumulocity.mibparser.model.DeviceType;
-import com.cumulocity.mibparser.model.Register;
 import com.cumulocity.mibparser.model.MibUploadResult;
+import com.cumulocity.mibparser.model.Register;
 import com.cumulocity.mibparser.service.MibParserService;
 import lombok.extern.slf4j.Slf4j;
-import net.percederberg.mibble.*;
+import net.percederberg.mibble.Mib;
+import net.percederberg.mibble.MibLoader;
+import net.percederberg.mibble.MibLoaderException;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
--- test/java/com/cumulocity/mibparser/MibParserServiceTests.java
+++ test/java/com/cumulocity/mibparser/MibParserServiceTests.java
@@ -1,9 +1,16 @@
+/*
+ * Copyright (c) 2019 Cumulocity GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.cumulocity.mibparser;
 
-import com.cumulocity.mibparser.model.Register;
 import com.cumulocity.mibparser.service.impl.MibParserServiceImpl;
-import net.percederberg.mibble.Mib;
-import net.percederberg.mibble.MibSymbol;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,10 +19,11 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
