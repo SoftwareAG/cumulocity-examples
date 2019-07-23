a91eebee4be71de3bbb5b796c1fdacffabd8a21c
kczu <kczu@kczu-HP-EliteBook-850-G5>
2019-07-23 11:32:02 +0200

    MTM-27487 add maven offline to cumulocity examples v3

--- .jenkins/scripts/compile.sh
+++ .jenkins/scripts/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 ./mvnw clean install -s $MVN_SETTINGS -U -DskipTests -Dskip.agent.package.container=true -Dskip.microservice.package=true -T 4
-mvn de.qaware.maven:go-offline-maven-plugin:resolve-dependencies
+./mvnw de.qaware.maven:go-offline-maven-plugin:resolve-dependencies
