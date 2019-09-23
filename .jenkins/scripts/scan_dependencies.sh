60d10f1076a97ddd39baffd764d3441babfa75d1
Kamil Sobala <kamil.sobala@cumulocity.com>
2019-09-23 11:40:11 +0200

    MTM-28527: Run third party dependencies scan within each release

--- .jenkins/scripts/scan_dependencies.sh
+++ .jenkins/scripts/scan_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 MVN_GOAL="com.nsn.cumulocity.dependencies:3rd-license-maven-plugin:3rd-tpp-fetcher-scan"
+if [ -z "$TPP_FETCHER_URL" ]; then TPP_FETCHER_URL="http://172.30.0.129:8083"; fi
 TPP_URL_PARAM="-Dtpp.fetcher.url=$TPP_FETCHER_URL"
 ENABLE_SCAN="-Dtpp.fetcher.scan.enabled=true"
 MVN_SCAN_TPP="$MVN_GOAL $TPP_URL_PARAM $ENABLE_SCAN"
