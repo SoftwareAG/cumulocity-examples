5024ad53924e6918343385f0cc9f7c5dd2c41f58
Kamil Sobala <kamil.sobala@cumulocity.com>
2019-11-18 16:07:24 +0100

    MTM-28174: Migrate cumulocity-examples to at least Java 8. Code cleanup.

--- hello-world-microservice/src/main/java/c8y/example/helloworld/HelloWorldMain.java
+++ hello-world-microservice/src/main/java/c8y/example/helloworld/HelloWorldMain.java
@@ -2,9 +2,6 @@ package c8y.example.helloworld;
 
 import com.cumulocity.microservice.autoconfigure.MicroserviceApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +17,6 @@ public class HelloWorldMain {
     @RequestMapping("hello")
     public String greeting(@RequestParam(value = "who", defaultValue = "world") String who) {
         return "hello " + who + "!";
-    }
-
-    @RequestMapping("test/{param}")
-    public ResponseEntity<String> test(@PathVariable(name = "param") String param) {
-        return new ResponseEntity<>("Param was: " + param, HttpStatus.OK);
     }
 
 }
