aaf38604e23ece5b45f1e4ce37b6c8d1f9b85dd1
Kamil Sobala <kamil.sobala@cumulocity.com>
2020-02-21 13:22:12 +0100

    [MTM-30025]: Scan dependencies of impact hex&json processors

--- .jenkins/scripts/scan_dependencies.sh
+++ .jenkins/scripts/scan_dependencies.sh
@@ -10,3 +10,5 @@ ENABLE_SCAN="-Dtpp.fetcher.scan.enabled=true"
 MVN_SCAN_TPP="$MVN_GOAL $TPP_URL_PARAM $ENABLE_SCAN"
 
 ./mvnw -f ./tracker-agent/pom.xml $MVN_SCAN_TPP -Dtpp.fetcher.project.name=tracker-agent 
+./mvnw -f ./sample-hex-decoder/pom.xml $MVN_SCAN_TPP -Dtpp.fetcher.project.name=impact-custom-processor
+./mvnw -f ./sample-json-decoder/pom.xml $MVN_SCAN_TPP -Dtpp.fetcher.project.name=impact-custom-processor
