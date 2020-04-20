3136d2eae0c01376cdc688665838db7d987e3770
Lukasz Piasecki <lukasz.piasecki@softwareag.com>
2020-04-20 17:48:49 +0200

    noissue: changed snmp rpm name to fix the deploy script

--- .jenkins/scripts/deploy.sh
+++ .jenkins/scripts/deploy.sh
@@ -16,7 +16,7 @@ YUM_USR_KEY=~/.ssh/id_rsa_hudson
 YUM_SRV=yum.cumulocity.com
 deploy() {
     echo "deploy $1 to $YUM_SRV:${YUM_DEST_DIR}"
-    scp -Cr -i ${YUM_USR_KEY} $1 ${YUM_USR}@${YUM_SRV}:${YUM_DEST_DIR}
+    scp -Cr -i ${YUM_USR_KEY} $1 ${YUM_USR}@${YUM_SRV}:${YUM_DEST_DIR} | true
 }
 
 if [ "!$1" = "!release" ]
@@ -24,6 +24,6 @@ then
     # Copy snmp-agent-gateway RPM and snmp-mib-parser's zip files to examples/snmp folder
     # Copy only the released files not the SNAPSHOT files
     YUM_DEST_DIR=/var/www/resources/examples/snmp
-    deploy $(find ./ -regextype egrep   -regex ".*snmp-agent-gateway-[0-9]+\.[0-9]+\.[0-9]+\.noarch.rpm")
+    deploy $(find ./ -regextype egrep   -regex ".*snmp-agent-gateway-[0-9]+\.[0-9]+\.[0-9]+\.rpm")
     deploy $(find ./ -regextype egrep   -regex ".*snmp-mib-parser-[0-9]+\.[0-9]+\.[0-9]+\.zip")
 fi
