5989d93f028c38ee768a55a7893b6e6efee692c9
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2021-02-11 15:12:03 +0000

    Merged in bugfix/MTM-35567/1007.0.0-migrate-to-new-jenkins-infrastructure (pull request #37)
    MTM-35567: migrate to new jenkins version
    
    * MTM-35567: migrate to new jenkins version
    
    Approved-by: Łukasz P
    Approved-by: Wojciech Bednarczyk

--- .jenkins/scripts/deploy.sh
+++ .jenkins/scripts/deploy.sh
@@ -2,22 +2,34 @@
 set -e
 # ./mvnw clean deploy -DskipTests -s $MVN_SETTINGS
 #/var/lib/jenkins/bin/deploy2yum.sh -p $(find ./ -name *.rpm)
+
+YUM_USR=hudson
+YUM_SRV=yum.cumulocity.com
+deployAll(){
+   for i in "$@"
+   do
+      deploy $i
+  done
+}
+
+deploy() {
+  echo "deploy $1 to $YUM_SRV:${YUM_DEST_DIR}"
+  scp -o StrictHostKeyChecking=no  -Cr $1 ${YUM_USR}@${YUM_SRV}:${YUM_DEST_DIR} | true
+}
+setLatest(){
+  echo "update latest $2 to $1"
+  ssh ${YUM_USR}@${YUM_SRV} ln -sf $1 $2/$3-latest.$4
+}
 if [ "!$1" = "!release" ]
 then
-    /var/jenkins_home/bin/deploy2yum.sh -p $(find ./ -name *.rpm| grep -v SNAPSHOT)
+    YUM_DEST_DIR=/var/incoming-rpms/cumulocity/
+    deployAll $(find ./ -name *.rpm| grep -v SNAPSHOT)
 fi
 if [ "!$1" = "!snapshot" ]
 then
-    /var/jenkins_home/bin/deploy2yum.sh $(find ./ -name *.rpm)
+    YUM_DEST_DIR=/var/incoming-rpms/cumulocity-testing/
+    deployAll $(find ./ -name *.rpm)
 fi
-
-YUM_USR=hudson
-YUM_USR_KEY=~/.ssh/id_rsa_hudson
-YUM_SRV=yum.cumulocity.com
-deploy() {
-    echo "deploy $1 to $YUM_SRV:${YUM_DEST_DIR}"
-    scp -Cr -i ${YUM_USR_KEY} $1 ${YUM_USR}@${YUM_SRV}:${YUM_DEST_DIR} | true
-}
 
 YUM_DEST_DIR=/var/www/resources/kubernetes-images
 deploy $(find ./ -regextype egrep   -regex ".*snmp-mib-parser-[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?\.zip")
@@ -29,4 +41,18 @@ then
     YUM_DEST_DIR=/var/www/resources/examples/snmp
     deploy $(find ./ -regextype egrep   -regex ".*snmp-agent-gateway-[0-9]+\.[0-9]+\.[0-9]+-1\.noarch\.rpm")
     deploy $(find ./ -regextype egrep   -regex ".*snmp-mib-parser-[0-9]+\.[0-9]+\.[0-9]+\.zip")
+
+    YUM_DEST_DIR=/var/www/resources/examples
+    target_package=$(find ./ -regextype egrep -regex ".*cumulocity-linux-agent-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz")
+    target_package_name=$(basename $target_package)
+
+
+        if [ -z "$target_package" ] || [ -z "$YUM_DEST_DIR" ]; then
+            echo "cumulocity-linux-agent or destination  not find!"
+            exit 1
+        else
+            deploy $target_package
+            setLatest  $target_package_name $YUM_DEST_DIR  "cumulocity-linux-agent" "tar.gz"
+        fi
+
 fi
