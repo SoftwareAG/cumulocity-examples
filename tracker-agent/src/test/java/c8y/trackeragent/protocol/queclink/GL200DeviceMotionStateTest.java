3568377bdd3ae81681a636f47769cb478cc2fc02
RMIR <rahul.miryala@softwareag.com>
2021-05-28 23:20:02 +0530

    MTM-37383-Added copyright statement

--- tracker-agent/src/test/java/c8y/trackeragent/protocol/queclink/GL200DeviceMotionStateTest.java
+++ tracker-agent/src/test/java/c8y/trackeragent/protocol/queclink/GL200DeviceMotionStateTest.java
@@ -1,21 +1,8 @@
 /*
- * Copyright (C) 2013 Cumulocity GmbH
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of 
- * this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2012-2020 Cumulocity GmbH
+ * Copyright (c) 2021 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA,
+ * and/or its subsidiaries and/or its affiliates and/or their licensors. Use, reproduction, transfer, publication
+ * or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG.
  */
 
 package c8y.trackeragent.protocol.queclink;
