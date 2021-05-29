0f973e12ee712f622f09e72550b8c6e6da7192b6
RMIR <rahul.miryala@softwareag.com>
2021-05-29 12:23:25 +0530

    MTM-37383-Added copyright statements for tracker-agent

--- tracker-agent/src/main/java/c8y/trackeragent/service/MeasurementService.java
+++ tracker-agent/src/main/java/c8y/trackeragent/service/MeasurementService.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2012-2020 Cumulocity GmbH
+ * Copyright (c) 2021 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA,
+ * and/or its subsidiaries and/or its affiliates and/or their licensors.
+ *
+ * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided
+ * for in your License Agreement with Software AG.
+ */
+
 package c8y.trackeragent.service;
 
 import java.math.BigDecimal;
