3be20055e2540c1e47759b034ee8458817140a34
Rohit Joshi <rohit.joshi@cumulocity.com>
2021-06-07 17:44:48 +0530

    MTM-36527 token api integration

--- hello-world-notification-microservice/src/main/java/c8y/example/notification/helloworld/NotificationService.java
+++ hello-world-notification-microservice/src/main/java/c8y/example/notification/helloworld/NotificationService.java
@@ -2,10 +2,15 @@ package c8y.example.notification.helloworld;
 
 import c8y.example.notification.helloworld.websocket.NotificationCallback;
 import c8y.example.notification.helloworld.websocket.NotificationConsumerWebSocket;
+import com.cumulocity.microservice.subscription.model.MicroserviceSubscriptionAddedEvent;
+import com.cumulocity.rest.representation.reliable.notification.NotificationTokenRequestRepresentation;
+import com.cumulocity.sdk.client.Platform;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -17,16 +22,25 @@ import java.util.List;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class NotificationService {
 
-    @PostConstruct
+    private final Platform platform;
+
+    @EventListener
+    public void onSubscriptionAdded(MicroserviceSubscriptionAddedEvent event) throws InterruptedException, IOException, URISyntaxException {
+        init();
+    }
+
     public void init() throws URISyntaxException, IOException, InterruptedException {
 
         String token = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJzdWIiLCJ0b3BpYyI6Im1hbmFnZW1lbnQvcmVsbm90aWYvdGVzdHN1YnNjcmlwdGlvbiIsImp0aSI6ImExZGI0MjE0LWJmMmEtNGNmNy04OWJiLTliZDM3YTY1OWVjZCIsImlhdCI6MTYyMzA1MzU5OSwiZXhwIjoxNjI5MDUzNTk5fQ.CKH_Vn9IWKFKzZWkW-Gomck9QblA1pKnxjBbu6nJLIGFF6dsgrXQpSUjJM-HNu2Y3WXcglul-QWJH3oErUwh3l2hPI0pD7XPBlUjinzEfFLtg4-aiYw0Uhkmr1lURqVu3I_qckLxnk_EWR81lc676j8SM28v9Y0TLYA_CUkfwrN-47-BS6dBINTkCsPGl0NfI41VXE8tejCBxZXCEFYhgeqSjmAB6ZMzWAiAaFocpqiT81zJSSnyhIeqos1ZwuOwF_MdE3iud1LJbw2cfZJXM5Nlz1Zt-A9ctoxb1y-qo3QYxgql7ANuUPzYa6Ng2svz2iuYS70Rg9kOXRT4dkiWdA";
         String webSocketUrl = "ws://localhost:8080/c8y/relnotif/consumer/?token=" + token;
 
-        NotificationConsumerWebSocket socket = new NotificationConsumerWebSocket(new NotificationCallback() {
+        NotificationTokenRequestRepresentation representation = new NotificationTokenRequestRepresentation("sub", "testsubscription", 1440, false);
+        log.info("TOKEN: " + platform.getTokenApi().create(representation));
 
+        NotificationConsumerWebSocket socket = new NotificationConsumerWebSocket(new NotificationCallback() {
             @Override
             public void onNotification(List<String> headers, String notification) {
                 for (String header : headers) {
