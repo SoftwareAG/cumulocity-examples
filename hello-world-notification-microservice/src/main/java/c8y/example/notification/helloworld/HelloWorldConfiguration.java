403ddfbbacb01e0dd6ad7485c51e5e3e41726a9e
Rohit Joshi <rohit.joshi@cumulocity.com>
2021-06-07 14:53:01 +0530

    MTM-36527 base project

---
+++ hello-world-notification-microservice/src/main/java/c8y/example/notification/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,8 @@
+package c8y.example.notification.helloworld;
+
+import com.cumulocity.microservice.autoconfigure.MicroserviceApplication;
+
+@MicroserviceApplication
+public class HelloWorldConfiguration {
+
+}
