b9c728c6f52cfc82a44c2a0c1a2e95972aaf4c24
Rohit Joshi <rohit.joshi@cumulocity.com>
2021-06-07 15:05:18 +0530

    MTM-36527 jetty websocket

---
+++ hello-world-notification-microservice/src/main/java/c8y/example/notification/helloworld/websocket/NotificationCallback.java
@@ -0,0 +1,20 @@
+package c8y.example.notification.helloworld.websocket;
+
+import java.util.List;
+
+/**
+ * Implement this interface to handle notifications.
+ */
+public interface NotificationCallback {
+    /**
+     * Called on receiving a notification. The notification will be acknowledged if no exception raised.
+     * @param headers the headers describing the notification.
+     * @param notification the notification as a JSON string.
+     */
+    void onNotification(List<String> headers, String notification);
+
+    /**
+     * Called on close of the underlying web socket connection. Normally, a reconnection should be attempted.
+     */
+    void close();
+}
