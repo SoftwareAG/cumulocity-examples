f1537d4a8661f250d675020af9886cf876ed0b9a
Rohit Joshi <rohit.joshi@cumulocity.com>
2021-06-08 13:02:53 +0530

    MTM-36527 added required role

--- hello-world-notification-microservice/src/main/java/c8y/example/notification/helloworld/NotificationService.java
+++ hello-world-notification-microservice/src/main/java/c8y/example/notification/helloworld/NotificationService.java
@@ -25,20 +25,22 @@ import java.util.List;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class NotificationService {
 
+    // TODO TokenApi cannot be injected - investigate
     private final Platform platform;
 
     @EventListener
     public void onSubscriptionAdded(MicroserviceSubscriptionAddedEvent event) throws InterruptedException, IOException, URISyntaxException {
+        log.info("Subscription added for Tenant ID: <{}> ", event.getCredentials().getTenant());
         init();
     }
 
     public void init() throws URISyntaxException, IOException, InterruptedException {
 
-        String token = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJzdWIiLCJ0b3BpYyI6Im1hbmFnZW1lbnQvcmVsbm90aWYvdGVzdHN1YnNjcmlwdGlvbiIsImp0aSI6ImExZGI0MjE0LWJmMmEtNGNmNy04OWJiLTliZDM3YTY1OWVjZCIsImlhdCI6MTYyMzA1MzU5OSwiZXhwIjoxNjI5MDUzNTk5fQ.CKH_Vn9IWKFKzZWkW-Gomck9QblA1pKnxjBbu6nJLIGFF6dsgrXQpSUjJM-HNu2Y3WXcglul-QWJH3oErUwh3l2hPI0pD7XPBlUjinzEfFLtg4-aiYw0Uhkmr1lURqVu3I_qckLxnk_EWR81lc676j8SM28v9Y0TLYA_CUkfwrN-47-BS6dBINTkCsPGl0NfI41VXE8tejCBxZXCEFYhgeqSjmAB6ZMzWAiAaFocpqiT81zJSSnyhIeqos1ZwuOwF_MdE3iud1LJbw2cfZJXM5Nlz1Zt-A9ctoxb1y-qo3QYxgql7ANuUPzYa6Ng2svz2iuYS70Rg9kOXRT4dkiWdA";
-        String webSocketUrl = "ws://localhost:8080/c8y/relnotif/consumer/?token=" + token;
-
-        NotificationTokenRequestRepresentation representation = new NotificationTokenRequestRepresentation("sub", "testsubscription", 1440, false);
-        log.info("TOKEN: " + platform.getTokenApi().create(representation));
+        final NotificationTokenRequestRepresentation representation = new NotificationTokenRequestRepresentation("sub", "testsubscription", 1440, false);
+        //final String token = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJzdWIiLCJ0b3BpYyI6Im1hbmFnZW1lbnQvcmVsbm90aWYvdGVzdHN1YnNjcmlwdGlvbiIsImp0aSI6ImExZGI0MjE0LWJmMmEtNGNmNy04OWJiLTliZDM3YTY1OWVjZCIsImlhdCI6MTYyMzA1MzU5OSwiZXhwIjoxNjI5MDUzNTk5fQ.CKH_Vn9IWKFKzZWkW-Gomck9QblA1pKnxjBbu6nJLIGFF6dsgrXQpSUjJM-HNu2Y3WXcglul-QWJH3oErUwh3l2hPI0pD7XPBlUjinzEfFLtg4-aiYw0Uhkmr1lURqVu3I_qckLxnk_EWR81lc676j8SM28v9Y0TLYA_CUkfwrN-47-BS6dBINTkCsPGl0NfI41VXE8tejCBxZXCEFYhgeqSjmAB6ZMzWAiAaFocpqiT81zJSSnyhIeqos1ZwuOwF_MdE3iud1LJbw2cfZJXM5Nlz1Zt-A9ctoxb1y-qo3QYxgql7ANuUPzYa6Ng2svz2iuYS70Rg9kOXRT4dkiWdA";
+        final String token = platform.getTokenApi().create(representation).getTokenString();
+        final String webSocketUrl = "ws://localhost:8080/c8y/relnotif/consumer/?token=" + token;
+        log.info("TOKEN: " + token);
 
         NotificationConsumerWebSocket socket = new NotificationConsumerWebSocket(new NotificationCallback() {
             @Override
