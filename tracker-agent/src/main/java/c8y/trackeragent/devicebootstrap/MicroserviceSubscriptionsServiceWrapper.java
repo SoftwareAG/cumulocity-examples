7bcb2250a42d216b813598e11fb04d43923f9181
mariuszkoceba <mariusz.koceba@softwareag.com>
2021-07-16 11:03:21 +0200

    MTM-38993: (tracker-agent) - fixed validating subscription

--- tracker-agent/src/main/java/c8y/trackeragent/devicebootstrap/MicroserviceSubscriptionsServiceWrapper.java
+++ tracker-agent/src/main/java/c8y/trackeragent/devicebootstrap/MicroserviceSubscriptionsServiceWrapper.java
@@ -3,12 +3,15 @@ package c8y.trackeragent.devicebootstrap;
 import c8y.trackeragent.exception.TenantNotSubscribedException;
 import com.cumulocity.microservice.context.credentials.MicroserviceCredentials;
 import com.cumulocity.microservice.subscription.service.MicroserviceSubscriptionsService;
+import com.cumulocity.sdk.client.SDKException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.concurrent.Callable;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MicroserviceSubscriptionsServiceWrapper {
@@ -30,7 +33,13 @@ public class MicroserviceSubscriptionsServiceWrapper {
     }
 
     private void validateSubscription(String tenant) {
-        if (microserviceSubscriptionsService.getCredentials(tenant).isEmpty()) {
+        try {
+            if (microserviceSubscriptionsService.getCredentials(tenant).isEmpty()) {
+                log.error("There is no subscription for tenant: {}", tenant);
+                throw new TenantNotSubscribedException("There is no subscription for tenant: " + tenant);
+            }
+        } catch (SDKException e) {
+            log.error("There is no subscription for tenant: {}", tenant, e);
             throw new TenantNotSubscribedException("There is no subscription for tenant: " + tenant);
         }
     }
