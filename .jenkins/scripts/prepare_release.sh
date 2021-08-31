56e594d27fb711e76d6d4750b6e5ed25f0fb7a37
Kamil Sobala <kamil.sobala@cumulocity.com>
2021-08-31 10:04:20 +0000

    Merged in feature/MTM-40644/c8y-examples_github (pull request #68)
    [MTM-40644]: Migrate cumulocity-examples to github
    
    Approved-by: Jakub Kotorowicz
    Approved-by: Damian Mariak

--- .jenkins/scripts/prepare_release.sh
+++ .jenkins/scripts/prepare_release.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -e
 
 release_version=$1
 echo "equivalent of git flow release start $release_version"
 git checkout -b release/r${release_version}
 git commit --allow-empty -am "flow: Created branch release/r${release_version}"
-git push https://${BITBUCKET_USER}:${BITBUCKET_PASSWORD}@bitbucket.org/m2m/cumulocity-examples release/r${release_version}
+git push ${REPOSITORY_BASE_URL}/cumulocity-examples release/r${release_version}
