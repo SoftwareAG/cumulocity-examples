51c70d3afd0c18704fab82277c0638d0617c4cab
Kamil Sobala <kamil.sobala@cumulocity.com>
2021-09-24 17:12:09 +0200

    [MTM-40644]: Use credentials with write access for cumulocity-examples release

--- .jenkins/scripts/prepare_release.sh
+++ .jenkins/scripts/prepare_release.sh
@@ -5,4 +5,4 @@ release_version=$1
 echo "equivalent of git flow release start $release_version"
 git checkout -b release/r${release_version}
 git commit --allow-empty -am "flow: Created branch release/r${release_version}"
-git push https://${REPOSITORY_USER}:${REPOSITORY_PASSWORD}@${REPOSITORY_BASE_URL}/cumulocity-examples release/r${release_version}
+git push https://${REPOSITORY_CREDENTIALS}@${REPOSITORY_BASE_URL}/cumulocity-examples release/r${release_version}
