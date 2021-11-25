f7c18d295106e00e51930033ff0b163cdb1f8952
atul <atulkumar.panda@cumulocity.com>
2021-11-25 20:44:52 +0530

    Updated decoder response.

--- lpwan-codec-microservice/src/main/java/c8y/example/App.java
+++ lpwan-codec-microservice/src/main/java/c8y/example/App.java
@@ -4,8 +4,8 @@ import com.cumulocity.lpwan.codec.annotation.CodecMicroserviceApplication;
 import org.springframework.boot.SpringApplication;
 
 @CodecMicroserviceApplication
-public class CodecMain {
+public class App {
     public static void main (String[] args) {
-        SpringApplication.run(CodecMain.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
--- lpwan-codec-microservice/src/main/java/c8y/example/service/DeviceCodecDecoder.java
+++ lpwan-codec-microservice/src/main/java/c8y/example/service/DeviceCodecDecoder.java
@@ -1,6 +1,5 @@
 package c8y.example.service;
 
-import c8y.example.util.DecodedObject;
 import com.cumulocity.lpwan.codec.Decoder;
 import com.cumulocity.lpwan.codec.exception.DecoderException;
 import com.cumulocity.lpwan.codec.model.DecoderInput;
@@ -39,7 +38,6 @@ public class DeviceCodecDecoder implements Decoder {
     }
 
     private void setUpMeasurementProperties(DecoderOutput decoderOutput, DecoderInput decoderInput) {
-//        List<MeasurementRepresentation> measurements = new ArrayList<>();
         Map<String, Object> valuesMap = new HashMap<>();
         valuesMap.put("value",15);
         valuesMap.put("unit","C");
@@ -52,8 +50,7 @@ public class DeviceCodecDecoder implements Decoder {
         measurement.setType("c8y_Temperature");
         measurement.setDateTime(new DateTime(decoderInput.getUpdateTime()));
         measurement.setProperty("c8y_Temperature", seriesMap);
-        decoderOutput.addMeasurement(measurement);
-//        measurements.add(measurement);
+        decoderOutput.addMeasurementToCreate(measurement);
 
         Map<String, Object> seriesMap_2 = new HashMap<>();
         seriesMap_2.put("E",20);
@@ -63,32 +60,21 @@ public class DeviceCodecDecoder implements Decoder {
         measurement_2.setType("c8y_DummyFragment");
         measurement_2.setDateTime(new DateTime(decoderInput.getUpdateTime()));
         measurement_2.setProperty("c8y_DummyFragment", seriesMap_2);
-        decoderOutput.addMeasurement(measurement_2);
-//        measurements.add(measurement_2);
-
-//        decoderOutput.setMeasurementsToCreate(measurements);
+        decoderOutput.addMeasurementToCreate(measurement_2);
     }
 
     private void setUpDeviceProperties(DecoderOutput decoderOutput) {
-//        List<ManagedObjectProperty> managedObjectProperties = new ArrayList<>();
-//        managedObjectProperties.add(new ManagedObjectProperty("deviceSample", "#sampleValue"));
-//        managedObjectProperties.add(new ManagedObjectProperty("newDeviceSample", 11,"C"));
-        decoderOutput.addManagedObjectProperty(new ManagedObjectProperty("deviceSample", "#sampleValue"));
-        decoderOutput.addManagedObjectProperty(new ManagedObjectProperty("newDeviceSample", 11,"C"));
+        decoderOutput.addPropertyToUpdateDeviceMo(new ManagedObjectProperty("deviceSample", "#sampleValue"));
+        decoderOutput.addPropertyToUpdateDeviceMo(new ManagedObjectProperty("newDeviceSample", 11,"C"));
 
         List<ManagedObjectProperty> childProperties = new ArrayList<>();
         childProperties.add(new ManagedObjectProperty("childDeviceSample", "#sampleValue_1"));
         childProperties.add(new ManagedObjectProperty("newChildDeviceSample", 110,"F"));
 
-//        managedObjectProperties.add(new ManagedObjectProperty("newChildDeviceSample", childProperties));
-        decoderOutput.addManagedObjectProperty(new ManagedObjectProperty("newChildDeviceSample", childProperties));
-
-//        decoderOutput.setPropertiesToUpdateDeviceMo(managedObjectProperties);
+        decoderOutput.addPropertyToUpdateDeviceMo(new ManagedObjectProperty("newChildDeviceSample", childProperties));
     }
 
     private void setUpAlarmProperties(DecoderOutput decoderOutput, DecoderInput decoderInput) {
-//        List<AlarmRepresentation> alarms =  new ArrayList<>();
-
         AlarmRepresentation alarm = new AlarmRepresentation();
         alarm.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
         alarm.setType("Type_1");
@@ -96,8 +82,7 @@ public class DeviceCodecDecoder implements Decoder {
         alarm.setSeverity("WARNING");
         alarm.setStatus(CumulocityAlarmStatuses.ACTIVE.name());
         alarm.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        decoderOutput.addAlarm(alarm);
-//        alarms.add(alarm);
+        decoderOutput.addAlarmToCreate(alarm);
 
         AlarmRepresentation alarm_2 = new AlarmRepresentation();
         alarm_2.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
@@ -106,10 +91,7 @@ public class DeviceCodecDecoder implements Decoder {
         alarm_2.setSeverity("CRITICAL");
         alarm_2.setStatus(CumulocityAlarmStatuses.ACTIVE.name());
         alarm_2.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        decoderOutput.addAlarm(alarm_2);
-//        alarms.add(alarm_2);
-
-//        decoderOutput.setAlarmsToCreate(alarms);
+        decoderOutput.addAlarmToCreate(alarm_2);
     }
 
     private void setUpAlarmTypesToClearProperties(DecoderOutput decoderOutput) {
@@ -117,24 +99,18 @@ public class DeviceCodecDecoder implements Decoder {
     }
 
     private void setUpEventProperties(DecoderOutput decoderOutput, DecoderInput decoderInput) {
-//        List<EventRepresentation> events = new ArrayList<>();
-
         EventRepresentation event = new EventRepresentation();
         event.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
         event.setText("Event_Text");
         event.setType("Event_Type");
         event.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        decoderOutput.addEvent(event);
-//        events.add(event);
+        decoderOutput.addEventToCreate(event);
 
         EventRepresentation event_2 = new EventRepresentation();
         event_2.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
         event_2.setText("Event_Text_2");
         event_2.setType("Event_Type_2");
         event_2.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        decoderOutput.addEvent(event_2);
-//        events.add(event_2);
-
-//        decoderOutput.setEventsToCreate(events);
+        decoderOutput.addEventToCreate(event_2);
     }
 }
