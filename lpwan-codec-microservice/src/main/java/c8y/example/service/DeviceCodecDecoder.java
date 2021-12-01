ff2908a4a321e64c4908237af708f2f60a7f244a
atul <atulkumar.panda@cumulocity.com>
2021-12-01 15:43:31 +0530

    Provided realistic values for the manufacturer and model with sensible MEA properties Provided basic comments for the classes and the methods.

--- lpwan-codec-microservice/src/main/java/c8y/example/service/DeviceCodecDecoder.java
+++ lpwan-codec-microservice/src/main/java/c8y/example/service/DeviceCodecDecoder.java
@@ -1,7 +1,6 @@
 package c8y.example.service;
 
 import com.cumulocity.lpwan.codec.Decoder;
-import com.cumulocity.lpwan.codec.exception.DecoderException;
 import com.cumulocity.lpwan.codec.model.DecoderInput;
 import com.cumulocity.lpwan.codec.model.DecoderOutput;
 import com.cumulocity.lpwan.codec.model.ManagedObjectProperty;
@@ -14,103 +13,108 @@ import com.cumulocity.rest.representation.measurement.MeasurementRepresentation;
 import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class should provide the decoding logic for the device(s) and the relevant Measurements, Events, Alarms
+ * and the properties to update the managed object of the device.
+ */
 
 @Component
 public class DeviceCodecDecoder implements Decoder {
 
     @Override
-    public DecoderOutput decode(DecoderInput decoderInput) throws DecoderException {
+    public DecoderOutput decode(DecoderInput decoderInput) {
 
         DecoderOutput decoderOutput = new DecoderOutput();
 
         setUpMeasurementProperties(decoderOutput,decoderInput);
-
         setUpEventProperties(decoderOutput, decoderInput);
-
         setUpAlarmProperties(decoderOutput, decoderInput);
-
         setUpAlarmTypesToClearProperties(decoderOutput);
-
         setUpDeviceProperties(decoderOutput);
 
         return decoderOutput;
     }
 
     private void setUpMeasurementProperties(DecoderOutput decoderOutput, DecoderInput decoderInput) {
-        Map<String, Object> valuesMap = new HashMap<>();
-        valuesMap.put("value",15);
-        valuesMap.put("unit","C");
+        Map<String, Object> valuesMap_Temperature = new HashMap<>();
+        valuesMap_Temperature.put("value",45);
+        valuesMap_Temperature.put("unit","C");
 
-        Map<String, Object> seriesMap = new HashMap<>();
-        seriesMap.put("T",valuesMap);
+        Map<String, Object> seriesMap_Temperature = new HashMap<>();
+        seriesMap_Temperature.put("T",valuesMap_Temperature);
 
-        MeasurementRepresentation measurement = new MeasurementRepresentation();
-        measurement.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
-        measurement.setType("c8y_Temperature");
-        measurement.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        measurement.setProperty("c8y_Temperature", seriesMap);
-        decoderOutput.addMeasurementToCreate(measurement);
+        MeasurementRepresentation TemperatureMeasurement = new MeasurementRepresentation();
+        TemperatureMeasurement.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
+        TemperatureMeasurement.setType("c8y_Temperature");
+        TemperatureMeasurement.setDateTime(new DateTime(decoderInput.getUpdateTime()));
+        TemperatureMeasurement.setProperty("c8y_Temperature", seriesMap_Temperature);
+        decoderOutput.addMeasurementToCreate(TemperatureMeasurement);
 
-        Map<String, Object> seriesMap_2 = new HashMap<>();
-        seriesMap_2.put("E",20);
+        Map<String, Object> seriesMap_Humidity = new HashMap<>();
+        seriesMap_Humidity.put("H",80);
 
-        MeasurementRepresentation measurement_2 = new MeasurementRepresentation();
-        measurement_2.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
-        measurement_2.setType("c8y_DummyFragment");
-        measurement_2.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        measurement_2.setProperty("c8y_DummyFragment", seriesMap_2);
-        decoderOutput.addMeasurementToCreate(measurement_2);
+        MeasurementRepresentation humidityMeasurement = new MeasurementRepresentation();
+        humidityMeasurement.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
+        humidityMeasurement.setType("c8y_Humidity");
+        humidityMeasurement.setDateTime(new DateTime(decoderInput.getUpdateTime()));
+        humidityMeasurement.setProperty("c8y_Humidity", seriesMap_Humidity);
+        decoderOutput.addMeasurementToCreate(humidityMeasurement);
     }
 
     private void setUpDeviceProperties(DecoderOutput decoderOutput) {
-        decoderOutput.addPropertyToUpdateDeviceMo(new ManagedObjectProperty("deviceSample", "#sampleValue"));
-        decoderOutput.addPropertyToUpdateDeviceMo(new ManagedObjectProperty("newDeviceSample", 11,"C"));
+        decoderOutput.addPropertyToUpdateDeviceMo(new ManagedObjectProperty("c8y_SignalStrength", "LOW"));
+        decoderOutput.addPropertyToUpdateDeviceMo(new ManagedObjectProperty("c8y_BatteryLevel", 35,"mAH"));
 
-        List<ManagedObjectProperty> childProperties = new ArrayList<>();
-        childProperties.add(new ManagedObjectProperty("childDeviceSample", "#sampleValue_1"));
-        childProperties.add(new ManagedObjectProperty("newChildDeviceSample", 110,"F"));
+        List<ManagedObjectProperty> memoryUsageProperties = new ArrayList<>();
+        memoryUsageProperties.add(new ManagedObjectProperty("c8y_CpuUsage", "HIGH"));
+        memoryUsageProperties.add(new ManagedObjectProperty("c8y_Memory", 512,"MB"));
 
-        decoderOutput.addPropertyToUpdateDeviceMo(new ManagedObjectProperty("newChildDeviceSample", childProperties));
+        decoderOutput.addPropertyToUpdateDeviceMo(new ManagedObjectProperty("c8y_Memory", memoryUsageProperties));
     }
 
     private void setUpAlarmProperties(DecoderOutput decoderOutput, DecoderInput decoderInput) {
-        AlarmRepresentation alarm = new AlarmRepresentation();
-        alarm.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
-        alarm.setType("Type_1");
-        alarm.setText("Alarm_Text");
-        alarm.setSeverity("WARNING");
-        alarm.setStatus(CumulocityAlarmStatuses.ACTIVE.name());
-        alarm.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        decoderOutput.addAlarmToCreate(alarm);
+        AlarmRepresentation temperatureAlarm = new AlarmRepresentation();
+        temperatureAlarm.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
+        temperatureAlarm.setType("c8y_Temperature");
+        temperatureAlarm.setText("Temperature is going above the limit.");
+        temperatureAlarm.setSeverity("WARNING");
+        temperatureAlarm.setStatus(CumulocityAlarmStatuses.ACTIVE.name());
+        temperatureAlarm.setDateTime(new DateTime(decoderInput.getUpdateTime()));
+        decoderOutput.addAlarmToCreate(temperatureAlarm);
 
-        AlarmRepresentation alarm_2 = new AlarmRepresentation();
-        alarm_2.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
-        alarm_2.setType("Type_2");
-        alarm_2.setText("Alarm_Text_2");
-        alarm_2.setSeverity("CRITICAL");
-        alarm_2.setStatus(CumulocityAlarmStatuses.ACTIVE.name());
-        alarm_2.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        decoderOutput.addAlarmToCreate(alarm_2);
+        AlarmRepresentation humidityAlarm = new AlarmRepresentation();
+        humidityAlarm.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
+        humidityAlarm.setType("c8y_Humidity");
+        humidityAlarm.setText("Humidity is above the limit.");
+        humidityAlarm.setSeverity("CRITICAL");
+        humidityAlarm.setStatus(CumulocityAlarmStatuses.ACTIVE.name());
+        humidityAlarm.setDateTime(new DateTime(decoderInput.getUpdateTime()));
+        decoderOutput.addAlarmToCreate(humidityAlarm);
     }
 
     private void setUpAlarmTypesToClearProperties(DecoderOutput decoderOutput) {
-        decoderOutput.setAlarmTypesToClear(Arrays.asList("Type_1","Type_3"));
+        decoderOutput.addAlarmTypeToClear("c8y_Temperature");
+        decoderOutput.addAlarmTypeToClear("c8y_Speed");
     }
 
     private void setUpEventProperties(DecoderOutput decoderOutput, DecoderInput decoderInput) {
-        EventRepresentation event = new EventRepresentation();
-        event.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
-        event.setText("Event_Text");
-        event.setType("Event_Type");
-        event.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        decoderOutput.addEventToCreate(event);
+        EventRepresentation temperatureEvent = new EventRepresentation();
+        temperatureEvent.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
+        temperatureEvent.setText("Temperature is above the limit.");
+        temperatureEvent.setType("c8y_Temperature");
+        temperatureEvent.setDateTime(new DateTime(decoderInput.getUpdateTime()));
+        decoderOutput.addEventToCreate(temperatureEvent);
 
-        EventRepresentation event_2 = new EventRepresentation();
-        event_2.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
-        event_2.setText("Event_Text_2");
-        event_2.setType("Event_Type_2");
-        event_2.setDateTime(new DateTime(decoderInput.getUpdateTime()));
-        decoderOutput.addEventToCreate(event_2);
+        EventRepresentation humidityEvent = new EventRepresentation();
+        humidityEvent.setSource(ManagedObjects.asManagedObject(GId.asGId(decoderInput.getDeviceMoId())));
+        humidityEvent.setText("Humidity is above the limit");
+        humidityEvent.setType("c8y_Humidity");
+        humidityEvent.setDateTime(new DateTime(decoderInput.getUpdateTime()));
+        decoderOutput.addEventToCreate(humidityEvent);
     }
 }
