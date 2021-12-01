f95417f100c2002cc7b36d6e0a14015c2bca59b4
atul <atulkumar.panda@cumulocity.com>
2021-12-01 15:43:31 +0530

    Provided realistic values for the manufacturer and model with sensible MEA properties Provided basic comments for the classes and the methods.

--- lpwan-codec-microservice/src/main/java/c8y/example/service/DeviceCodecMicroservice.java
+++ lpwan-codec-microservice/src/main/java/c8y/example/service/DeviceCodecMicroservice.java
@@ -2,7 +2,6 @@ package c8y.example.service;
 
 import com.cumulocity.lpwan.codec.CodecMicroservice;
 import com.cumulocity.lpwan.codec.model.DeviceInfo;
-import com.cumulocity.lpwan.codec.model.DeviceTypeEnum;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -13,13 +12,29 @@ import java.util.stream.Stream;
 @Service
 public class DeviceCodecMicroservice extends CodecMicroservice {
 
+    /**
+     * This method should populate a set of unique devices identified by their manufacturer and model.
+     *
+     * @return Set<DeviceInfo>: A set of unique devices identified by their manufacturer and model.
+     */
     public Set<DeviceInfo> supportsDevices() {
-        DeviceInfo deviceInfo_X = new DeviceInfo("Dummy_Manufacturer_X", "Dummy_Model_X", DeviceTypeEnum.LORA);
-        DeviceInfo deviceInfo_Y = new DeviceInfo("Dummy_Manufacturer_Y", "Dummy_Model_Y", DeviceTypeEnum.SIGFOX);
-        return Stream.of(deviceInfo_X,deviceInfo_Y).collect(Collectors.toCollection(HashSet::new));
+
+        // The manufacturer "NKE" has 2 different devices with model "50-70-043" and "50-70-164"
+        DeviceInfo deviceInfo_NKE_50_70_043 = new DeviceInfo("NKE", "50-70-043");
+        DeviceInfo deviceInfo_NKE_50_70_164 = new DeviceInfo("NKE", "50-70-164");
+
+        // The manufacturer "LANSITEC" has 2 different devices with model "Outdoor Asset Tracker" and "Temperature Sensor"
+        DeviceInfo deviceInfo_Lansitec_Outdoor_Asset_Tracker = new DeviceInfo("LANSITEC", "Outdoor Asset Tracker");
+        DeviceInfo deviceInfo_Lansitec_Temperature_Sensor = new DeviceInfo("LANSITEC", "Temperature Sensor");
+
+        return Stream.of(deviceInfo_NKE_50_70_043, deviceInfo_NKE_50_70_164, deviceInfo_Lansitec_Outdoor_Asset_Tracker, deviceInfo_Lansitec_Temperature_Sensor).collect(Collectors.toCollection(HashSet::new));
     }
 
-    public String getMicroserviceContextPath(){
-        return "lpwancodecmicroservice";
+    /**
+     *
+     * @return String : The context path of the codec microservice
+     */
+    public String getMicroserviceContextPath() {
+        return "Nke-Lansitec-Decoder-Sample";
     }
 }
