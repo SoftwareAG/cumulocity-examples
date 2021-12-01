0b41b3ddfdc916bfd6e626f8aba67a65253754b6
atul <atulkumar.panda@cumulocity.com>
2021-12-01 21:36:09 +0530

    Lansitec Sample codec Microservice

---
+++ lora-codec-lansitec/src/main/java/lora/codec/lansitec/NearestBeaconAlgo.java
@@ -0,0 +1,36 @@
+package lora.codec.lansitec;
+
+import com.cumulocity.rest.representation.inventory.ManagedObjectRepresentation;
+
+import java.util.List;
+
+//@Component
+public class NearestBeaconAlgo extends Algo {
+
+    @Override
+    public Beacon getPosition(ManagedObjectRepresentation tracker, List<Beacon> beacons) {
+        Beacon beacon = null;
+        for (Beacon newBeacon : beacons) {
+            if (beacon != null) {
+                if (beacon.getMajor().equals(newBeacon.getMajor()) && beacon.getMinor().equals(newBeacon.getMinor()) || newBeacon.getRssi() > beacon.getRssi()) {
+                    beacon = newBeacon;
+                }
+            } else {
+                beacon = newBeacon;
+            }
+
+        }
+        return beacon;
+    }
+
+    @Override
+    public String getId() {
+        return "nearest";
+    }
+
+    @Override
+    public String getLabel() {
+        return "Nearest beacon";
+    }
+    
+}
