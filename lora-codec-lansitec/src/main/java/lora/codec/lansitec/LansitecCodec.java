99f3950ce4b2fb3f2a6c87a99756327f5e923f57
Ashwin Subramaniam Hariharan <ashh@softwareag.com>
2021-12-31 08:23:30 +0530

    Encoder Sample Microservice

--- lora-codec-lansitec/src/main/java/lora/codec/lansitec/LansitecCodec.java
+++ lora-codec-lansitec/src/main/java/lora/codec/lansitec/LansitecCodec.java
@@ -1,7 +1,12 @@
 package lora.codec.lansitec;
 
 import com.cumulocity.microservice.lpwan.codec.Codec;
+import com.cumulocity.microservice.lpwan.codec.model.DeviceCommand;
 import com.cumulocity.microservice.lpwan.codec.model.DeviceInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lora.codec.lansitec.encoder.LansitecEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
@@ -14,12 +19,39 @@ public class LansitecCodec implements Codec {
 
     /**
      * This method should populate a set of unique devices identified by their manufacturer and model.
+     *
      * @return Set: A set of unique devices identified by their manufacturer and model.
      */
     public Set<DeviceInfo> supportsDevices() {
 
         // The manufacturer "LANSITEC" has 2 different devices with model "Outdoor Asset Tracker" and "Temperature Sensor"
-        DeviceInfo deviceInfo_Lansitec_Asset_Tracker = new DeviceInfo("LANSITEC", "Asset Tracker");
+        DeviceCommand positionRequestCommand = new DeviceCommand(LansitecEncoder.POSITION_REQUEST, "Device Config", LansitecEncoder.POSITION_REQUEST);
+        DeviceCommand deviceRequestCommand = new DeviceCommand(LansitecEncoder.DEVICE_REQUEST, "Device Config", LansitecEncoder.DEVICE_REQUEST);
+        DeviceCommand registerRequestCommand = new DeviceCommand(LansitecEncoder.REGISTER_REQUEST, "Device Config", LansitecEncoder.REGISTER_REQUEST);
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode deviceOperationElements = mapper.createObjectNode();
+        deviceOperationElements.put("breakpoint",Boolean.TRUE);
+        deviceOperationElements.put("selfadapt",Boolean.TRUE);
+        deviceOperationElements.put("oneoff",Boolean.TRUE);
+        deviceOperationElements.put("alreport",Boolean.TRUE);
+        deviceOperationElements.put("pos",0);
+        deviceOperationElements.put("hb",0);
+        String json =null;
+        try {
+            json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(deviceOperationElements);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        DeviceCommand setConfigCommand = new DeviceCommand(LansitecEncoder.SET_CONFIG, "Device Config", json);
+
+        Set<DeviceCommand> deviceCommands = new HashSet<>();
+        deviceCommands.add(positionRequestCommand);
+        deviceCommands.add(deviceRequestCommand);
+        deviceCommands.add(registerRequestCommand);
+        deviceCommands.add(setConfigCommand);
+
+        DeviceInfo deviceInfo_Lansitec_Asset_Tracker = new DeviceInfo("LANSITEC", "Asset Tracker", deviceCommands);
         DeviceInfo deviceInfo_Lansitec_Temperature_Sensor = new DeviceInfo("LANSITEC", "Temperature Sensor");
 
         return Stream.of(deviceInfo_Lansitec_Asset_Tracker, deviceInfo_Lansitec_Temperature_Sensor).collect(Collectors.toCollection(HashSet::new));
--- lora-codec-lansitec/src/main/java/lora/codec/lansitec/encoder/LansitecEncoder.java
+++ lora-codec-lansitec/src/main/java/lora/codec/lansitec/encoder/LansitecEncoder.java
@@ -4,12 +4,69 @@ import com.cumulocity.microservice.customencoders.api.exception.EncoderServiceEx
 import com.cumulocity.microservice.customencoders.api.model.EncoderInputData;
 import com.cumulocity.microservice.customencoders.api.model.EncoderResult;
 import com.cumulocity.microservice.customencoders.api.service.EncoderService;
+import com.cumulocity.microservice.lpwan.codec.encoder.model.LpwanEncoderInputData;
+import com.cumulocity.microservice.lpwan.codec.encoder.model.LpwanEncoderResult;
+import com.cumulocity.model.idtype.GId;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.BaseEncoding;
 import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 @Component
 public class LansitecEncoder implements EncoderService {
+    public static final String SET_CONFIG = "set config";
+    public static final String DEVICE_REQUEST = "device request";
+    public static final String REGISTER_REQUEST = "register request";
+    public static final String POSITION_REQUEST = "position request";
+
     @Override
     public EncoderResult encode(EncoderInputData encoderInputData) throws EncoderServiceException {
-        return new EncoderResult();
+        LpwanEncoderInputData lpwanEncoderInputData = new LpwanEncoderInputData(GId.asGId(encoderInputData.getSourceDeviceId()),
+                encoderInputData.getCommandName(),
+                encoderInputData.getCommandData(),
+                encoderInputData.getArgs());
+
+        LpwanEncoderResult encoderResult = null;
+        if (lpwanEncoderInputData.getSourceDeviceInfo().getManufacturer().equalsIgnoreCase("Lansitec") && lpwanEncoderInputData.getSourceDeviceInfo().getModel().equals("Asset Tracker")) {
+            ObjectMapper mapper = new ObjectMapper();
+            String payload = null;
+            try {
+                if (lpwanEncoderInputData.getCommandName().equals(POSITION_REQUEST)) {
+                    payload = "A1FF";
+                } else if (lpwanEncoderInputData.getCommandName().equals(REGISTER_REQUEST)) {
+                    payload = "A2FF";
+                } else if (lpwanEncoderInputData.getCommandName().equals(DEVICE_REQUEST)) {
+                    payload = "A3FF";
+                } else if (lpwanEncoderInputData.getCommandName().equals(SET_CONFIG)) {
+                    JsonNode params = mapper.readTree(lpwanEncoderInputData.getCommandData());
+                    ByteBuffer buffer = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN);
+                    byte breakpoint = params.get("breakpoint").asBoolean() ? (byte) 8 : 0;
+                    byte selfadapt = params.get("selfadapt").asBoolean() ? (byte) 4 : 0;
+                    byte oneoff = params.get("oneoff").asBoolean() ? (byte) 2 : 0;
+                    byte alreport = params.get("alreport").asBoolean() ? (byte) 1 : 0;
+                    buffer.put((byte) ((byte) 0x90 | (byte) breakpoint | (byte) selfadapt | (byte) oneoff | (byte) alreport));
+                    buffer.putShort((short) params.get("pos").asInt());
+                    buffer.put((byte) params.get("hb").asInt());
+                    payload = BaseEncoding.base16().encode(buffer.array());
+                }
+
+                encoderResult = new LpwanEncoderResult(payload, 20);
+                encoderResult.setSuccess(true);
+                encoderResult.setMessage("Successfully Encoded the payload");
+            } catch (IOException e) {
+                e.printStackTrace();
+                encoderResult = new LpwanEncoderResult();
+                encoderResult.setSuccess(false);
+                encoderResult.setMessage("Encoding Payload Failed");
+            }
+        }
+        return encoderResult;
     }
 }
