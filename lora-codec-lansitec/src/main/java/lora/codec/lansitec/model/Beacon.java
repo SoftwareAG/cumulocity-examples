d4dde4ec7828043e4fc174fe60b35ec2dbf9e046
Ashwin Hariharan <92021838+ashhSAG93@users.noreply.github.com>
2022-01-19 11:22:19 +0530

    Mtm 42561 implement a sample codec microservice using the lpwan codec sdk (#39)
    MTM-41762: Custom codec support for LPWAN agents.
    
     * added lora-codec-lansitec microservice which implements the lpwan-custom-codec interfaces for encoding and decoding lora-lansitec devices (sample code)

---
+++ lora-codec-lansitec/src/main/java/lora/codec/lansitec/model/Beacon.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2012-2020 Cumulocity GmbH
+ * Copyright (c) 2022 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA,
+ * and/or its subsidiaries and/or its affiliates and/or their licensors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lora.codec.lansitec.model;
+
+public class Beacon {
+	private String major;
+	private String minor;
+	private int rssi;
+	public Beacon() {}
+	public Beacon(String major, String minor, int rssi) {
+		super();
+		this.major = major;
+		this.minor = minor;
+		this.rssi = rssi;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getMinor() {
+		return minor;
+	}
+	public void setMinor(String minor) {
+		this.minor = minor;
+	}
+	public int getRssi() {
+		return rssi;
+	}
+	public void setRssi(int rssi) {
+		this.rssi = rssi;
+	}
+}
