ffc762a18a32ee41668eb3ac7d606f93acf366f9
Dariusz Kaczynski <dkac@softwareag.com>
2022-03-23 21:50:46 +0100

    MTM-44678: Runs Maven in batch mode to improve build logs readability

--- .jenkins/scripts/compile.sh
+++ .jenkins/scripts/compile.sh
@@ -2,6 +2,6 @@
 set -e
 export MAVEN_OPTS="-Xmx2048m -XX:MetaspaceSize=1024m -XX:+TieredCompilation -XX:TieredStopAtLevel=1 ${MAVEN_OPTS}"
 
-./mvnw install -s $MVN_SETTINGS -U -DskipTests -Dskip.agent.package.container=true -Dskip.microservice.package=true "$@"
+./mvnw -B install -s $MVN_SETTINGS -U -DskipTests -Dskip.agent.package.container=true -Dskip.microservice.package=true "$@"
 
 # -Dchangelist=
