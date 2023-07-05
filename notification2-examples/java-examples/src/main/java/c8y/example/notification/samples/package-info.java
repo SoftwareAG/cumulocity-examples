2abe73c2169b02dd433da02d1d8cdc762eb7b695
Rohit Joshi <rohj@softwareag.com>
2023-07-05 14:48:09 +0100

    MTM-54278 Notification 2 examples

---
+++ notification2-examples/java-examples/src/main/java/c8y/example/notification/samples/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains examples that encourage best practices when working with Notifications 2.0 API.
+ *
+ * Example 1: Multiple “subscriptions” using the same topic (i.e., same subscription name)
+ * Example 2: Multiple consumers using the same topic and each getting a copy of the notifications
+ */
+
+package c8y.example.notification.samples;
+
